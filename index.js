7eaeb689da6e711fc72d7ec01eba3138122e3975
Jelani Cannon <jelani.cannon@gmail.com>
2022-09-20 12:21:55 -0700

    commit tree 199d3785b4232b5a529fc3e8becb97f7de5cca9a

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  }
+  else if (distance < 2500 && distance > 2000){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500) {
+    return "No can do."
+  }
+
+    
+  }
+
+
+
+function ternaryCheckCity(place){
+  const city = 'NYC'
+  const cityMessage = (place === city) ? "Ok, sounds good." : "No go.";
+  return cityMessage
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous" : return "Thank you so much.";
+    case "not as generous" : return "Thank you."
+    default : return "Bye."
+    
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
